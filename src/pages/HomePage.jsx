003ca53418de45c6b21d73239d12d0d6bb926c04
simone-sampino <s.samp98@gmail.com>
2025-08-07 19:53:13 +0200

    Refactor pages to use Jumbotron component for consistent layout and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Jumbotron from "../components/Jumbotron";
 
 export default function HomePage() {
-  const VITE_BACKEND_API_SERVER = import.meta.env.VITE_BACKEND_API_SERVER;
-
-  const api_url = `${VITE_BACKEND_API_SERVER}/movies`;
+  const api_url =
+    import.meta.env.VITE_BACKEND_API_SERVER +
+    import.meta.env.VITE_BACKEND_MOVIES_ENDPOINT;
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -17,42 +19,34 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="p-5 mb-4 bg-light rounded-3">
-        <div className="container-fluid py-5">
-          <h1 className="display-5 fw-bold">Home Page</h1>
-          <p className="col-md-8 fs-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-            sed sunt debitis, delectus perspiciatis doloremque cupiditate dolor
-            aut, ipsa accusantium culpa, quibusdam est nemo excepturi asperiores
-            magni autem obcaecati nostrum?
-          </p>
-          <button className="btn btn-primary btn-lg" type="button">
-            Example button
-          </button>
-        </div>
+      <Jumbotron
+        title="Home Page"
+        text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed sunt debitis, delectus perspiciatis doloremque cupiditate dolor aut, ipsa accusantium culpa, quibusdam est nemo excepturi asperiores magni autem obcaecati nostrum?"
+        cta="Example button"
+      />
 
-        <section className="container">
-          <div className="row row-cols-1 row-cols-md-3 g-3">
-            {movies.map((movie) => {
-              return (
-                <div key={movie.id} className="col">
-                  <div className="card">
-                    <img
-                      className="card-img-top"
-                      src={movie.image}
-                      alt={movie.title}
-                    ></img>
-                    <div className="card-body">
-                      <h3>{movie.title}</h3>
-                      <p>{movie.abstract}</p>
-                    </div>
+      <section className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-3">
+          {movies.map((movie) => {
+            return (
+              <div key={movie.id} className="col">
+                <div className="card h-100">
+                  <img
+                    className="card-img-top"
+                    src={import.meta.env.VITE_BACKEND_API_SERVER + movie.image}
+                    alt={movie.title}
+                  ></img>
+                  <div className="card-body">
+                    <h3>{movie.title}</h3>
+                    <p>{movie.abstract}</p>
+                    <Link to={`/movies/${movie.id}`}>Details</Link>
                   </div>
                 </div>
-              );
-            })}
-          </div>
-        </section>
-      </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage.jsx
@@ -1,7 +1,30 @@
+import { useParams } from "react-router-dom";
+import Jumbotron from "../components/Jumbotron";
+import { useEffect, useState } from "react";
+
 export default function SingleMoviePage() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const api_url = `${
+    import.meta.env.VITE_BACKEND_API_SERVER +
+    import.meta.env.VITE_BACKEND_MOVIES_ENDPOINT
+  }/${id}`;
+
+  useEffect(() => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+      });
+  }, []);
+
   return (
     <>
-      <h1>SingleMoviePage</h1>
+      <Jumbotron title={movie.title} text={movie.abstract} cta={movie.genre} />
+
+      <section>
+        <div className="container"></div>
+      </section>
     </>
   );
 }
